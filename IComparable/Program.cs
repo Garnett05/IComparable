00c3bc23179d900901dafe7d6effb7b52c8a92fd
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-06 00:52:35 -0300

    Início da resolução do problema. Todos os nomes são pegos do arquivo no diretório C e são armazenados em uma lista

---
+++ IComparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\alot.txt";
+
+            try
+            {
+                StreamReader sr = new StreamReader(sourcePath);
+                List<string> list = new List<string>();
+
+                while (!sr.EndOfStream)
+                {
+                    string s = sr.ReadLine();
+                    list.Add(s);                    
+                }
+                
+            }
+            catch (IOException e) {
+                Console.WriteLine("Um erro ocorreu: " + e.Message);
+            }
+        }
+    }
+}
